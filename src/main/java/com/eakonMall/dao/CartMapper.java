8779a85e3ab3f0013036dc5e739214ff93dbf9f4
EakonZhao <eakonzhao@gmail.com>
2017-05-10 16:05:16 +0800

    Completed the configuration of the project

---
+++ src/main/java/com/eakonMall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.eakonMall.dao;
+
+import com.eakonMall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/eakonMall/dao/ProductMapper.java
@@ -0,0 +1,19 @@
+package com.eakonMall.dao;
+
+import com.eakonMall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKeyWithBLOBs(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
